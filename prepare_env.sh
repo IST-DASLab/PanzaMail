4d4c929429bbe22681783ca4adee18ab2a5961cf
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-08-16 15:46:39 +0200

    install dependencies based on pyproject.toml instead of raw pip and conda commands

--- prepare_env.sh
+++ prepare_env.sh
@@ -4,10 +4,5 @@ trap 'trap - ERR RETURN; kill -INT $$ ; return' ERR RETURN
 conda create --name panza python=3.10 -y
 conda activate panza
 
-conda install pytorch==2.2.2 torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
-
-pip install langdetect langchain langchain-community sentence-transformers faiss-cpu fire mauve-text evaluate torchmetrics gradio cmake packaging nltk
-
-pip install git+https://github.com/IST-DASLab/llm-foundry
-pip install git+https://github.com/IST-DASLab/peft-rosa.git@grad_quant
-pip install spops_sm_80
+# install dependencies based on pyproject.toml
+pip install -e .
