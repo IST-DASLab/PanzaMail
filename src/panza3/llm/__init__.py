cd9b041d9587fdbecd86aa34eee27f0d7d0c83f2
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-08-28 13:02:27 +0200

    rename ollama_llm.py to ollama.py

--- src/panza3/llm/__init__.py
+++ src/panza3/llm/__init__.py
@@ -1,5 +1,5 @@
 from .base import LLM, ChatHistoryType, MessageType
 from .local import PeftLLM, TransformersLLM
-from .ollama_llm import OllamaLLM
+from .ollama import OllamaLLM
 
 __all__ = ["LLM", "ChatHistoryType", "MessageType", "OllamaLLM", "TransformersLLM", "PeftLLM"]
