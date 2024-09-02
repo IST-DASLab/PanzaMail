48ba916901dc40a25a0348cd69c686e085592e08
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-09-02 14:00:47 +0200

    comment out the running examples

--- src/panza3/run_panza.py
+++ src/panza3/run_panza.py
@@ -3,18 +3,20 @@ from llm import OllamaLLM, ChatHistoryType
 
 llm = OllamaLLM("custom", "path/to/file", {})
 
-messages: ChatHistoryType = [{"role": "user", "content": "Write a one-sentence email saying i will be late to the meeting"}]
+messages: ChatHistoryType = [
+    {"role": "user", "content": "Write a one-sentence email saying i will be late to the meeting"}
+]
 
 # Example of how to use the LLM with no streaming
-stream = llm.chat_stream(messages)
-while True:
-    try:
-        print(next(stream))
-    except StopIteration:
-        break
-    
+# stream = llm.chat_stream(messages)
+# while True:
+#     try:
+#         print(next(stream))
+#     except StopIteration:
+#         break
+
 # Example of how to use the LLM with streaming
-print(llm.chat(messages))
+# print(llm.chat(messages))
 
 # create a new PanzaWebService
 service = PanzaWebService()
