2e20f12c2e22b67dd3f0388160ce637b2a5d23d1
Eugenia Iofinova <eiofinov@ist.ac.at>
2024-09-16 15:56:14 +0200

    do NOT split test and train data by default

--- scripts/prepare_dataset.sh
+++ scripts/prepare_dataset.sh
@@ -2,7 +2,7 @@
 
 source config.sh
 
-TRAIN_RATIO=0.8
+TRAIN_RATIO=1.0
 SPLIT_TYPE="chronological"  # random or chronological
 
 CHUNK_SIZE=3000
@@ -21,6 +21,8 @@ do
    export "$KEY"="$VALUE"
 done
 
+
+
 USE_4BIT_QUANT=$([ "${LOAD_IN_4BIT}" = 1 ] && echo "--load-in-4bit" || echo "")
 USE_FP32_COMPUTE=$([ "${RUN_FP32}" = 1 ] && echo "--fp32" || echo "")
 
@@ -30,15 +32,31 @@ python ../src/panza/data_preparation/summarize_emails.py \
     --prompt-file="${PANZA_WORKSPACE}/src/panza/data_preparation/summarization_prompt.txt" \
     --batch-size=${PANZA_SUMMARIZATION_BATCH_SIZE} ${USE_4BIT_QUANT} ${USE_FP32_COMPUTE} &&
 
-# Create train and test splits
-python ../src/panza/data_preparation/split_data.py \
-    --data-path="${PANZA_DATA_DIR}/${PANZA_USERNAME}_clean_summarized.jsonl" \
-    --output-data-dir=${PANZA_DATA_DIR} \
-    --train-ratio=${TRAIN_RATIO} \
-    --split-type=${SPLIT_TYPE} \
-    --seed=${PANZA_SEED} &&
+if [[ $TRAIN_RATIO < 1.0 ]]; then
+    # Create train and test splits
+    SPLIT_PANZA_DATA_DIR=${PANZA_DATA_DIR}/split
+
+    python ../src/panza/data_preparation/split_data.py \
+        --data-path="${PANZA_DATA_DIR}/${PANZA_USERNAME}_clean_summarized.jsonl" \
+        --output-data-dir=${PANZA_DATA_DIR}/split \
+        --train-ratio=${TRAIN_RATIO} \
+        --split-type=${SPLIT_TYPE} \
+        --seed=${PANZA_SEED}
+
+    PANZA_DATA_DIR=$SPLIT_PANZA_DATA_DIR
+else
+    cp "${PANZA_DATA_DIR}/${PANZA_USERNAME}_clean_summarized.jsonl" \
+        "${PANZA_DATA_DIR}/train.jsonl"
+
+    # Finetuning requires some sort of test set, just use the training
+    # data again.
+    cp "${PANZA_DATA_DIR}/${PANZA_USERNAME}_clean_summarized.jsonl" \
+        "${PANZA_DATA_DIR}/test.jsonl"
+fi
 
 # Create vector store with emails embeddings
+# Note that if the data is split, then the PANZA_DATA_DIR,
+# where the vector store will be, will be the /split directory.
 python ../src/panza/data_preparation/create_vector_store.py \
     --path-to-emails="${PANZA_DATA_DIR}/train.jsonl" \
     --chunk-size=${CHUNK_SIZE} \
