ea5ffeae2482a22603d44cc0f222ca87bc193011
Eugenia Iofinova <eiofinov@ist.ac.at>
2024-10-21 13:02:40 +0200

    update env preparation script

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,8 +1,8 @@
 # crash in case of error
 trap 'trap - ERR RETURN; kill -INT $$ ; return' ERR RETURN
 
-conda create --name panza python=3.10 -y
-conda activate panza
+conda create --name panza_refactor python=3.10 -y
+conda activate panza_refactor
 
 # install dependencies based on pyproject.toml
-pip install -e .
+pip install -e .[training]
