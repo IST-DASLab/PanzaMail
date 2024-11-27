711b57d71918e0eb303a898d19897e544c7c1691
Eugenia Iofinova <eiofinov@ist.ac.at>
2024-11-27 13:18:48 +0100

    rm prepare_env.sh

--- prepare_env.sh
+++
@@ -1,8 +0,0 @@
-# crash in case of error
-trap 'trap - ERR RETURN; kill -INT $$ ; return' ERR RETURN
-
-conda create --name panza_refactor python=3.10 -y
-conda activate panza_refactor
-
-# install dependencies based on pyproject.toml
-pip install -e .[training]
